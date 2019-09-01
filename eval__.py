ef6e0bb91f540f603d514dbf25ea6ef3a51d5624
meoho <46700473+chemica1@users.noreply.github.com>
2019-09-01 21:10:27 +0900

    텍스트파일들은 좀 지우고살자 주영아

--- eval__.py
+++ eval__.py
@@ -3,21 +3,20 @@ import numpy as np
 #행렬의 덧셈과 뺄셈
 class cal:
 
+    r = np.array([[1, 2], [3, 4]])
+    p = np.array([[12, 11], [2, 4]])
+
     def add(self,A,B):
-        self.re=f'행렬A+행렬B: {A+B}'
-        print(self.re)
+        print(f'행렬A+행렬B: {(A+B)}')
 
     def sub(self,A,B):
-        self.re=f'행렬A-행렬B: {A-B}'
-        print(self.re)
+        print(f'행렬A-행렬B: {(A-B)}')
 
+    def main(self):
+        X= cal()
+        X.add(self.r, self.p)
+        X.sub(self.r, self.p)
 
-r=np.array([[1,2],[3,4]])
-p=np.array([[12,11],[2,4]])
-
-def main():
-    X= cal()
-    X.add(r,p)
-    X.sub(r,p)
-
-main()
+if __name__ == '__main__':
+    a = cal()
+    a.main()
