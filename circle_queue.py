c875baa7a55e44b8587cdeb57734a91a9566d5fb
chat-fuel <54631858+chat-fuel@users.noreply.github.com>
2019-09-08 16:34:35 +0900

    190908_16:34
    #circle_queue 껍질

---
+++ circle_queue.py
@@ -0,0 +1,49 @@
+class CircularQueue():
+
+    def __int__(self, max=1000):
+        self.max = max
+        self.queue = [None] * self.max
+        self.size = self.front = 0
+        self.rear = None
+
+    def is_empty(self):
+        return self.size == 0
+
+    def enqueue(self, data):
+        if self.is_full():
+            raise Exception("Queue is Full")
+
+        if self.rear == None:
+            self.rear = 0
+        else:
+            self.rear = self.next_index(self.rear)
+
+        self.queue[self.rear] = data
+        self.size += 1
+        return self.queue[self.rear]
+
+    def deque(self):
+        if self.is_empty():
+            raise
+            Exception('Queue is empty')
+            self.queue[self.front] = None
+            self.front = self.next_index(self.front)
+            return self.queue[self.front]
+
+    def display(self):
+        print(self.queue)
+
+if __name__ == '__main__':
+    cq = CircularQueue()
+    cq.dispaly()
+    print(cq.enqueu())
+    print(cq.enqueu())
+    print(cq.enqueu())
+    print(cq.enqueu())
+    cq.display()
+    print(cq.deque())
+    print(cq.deque())
+    cq.display()
+    print(cq.enqueue())
+    print(cq.enqueue())
+    cq.display()
--- eval__.py
+++ eval__.py
@@ -6,20 +6,6 @@ from time import sleep
 #행렬의 덧셈과 뺄셈
 class cal:
 
-    r = np.array([[1, 2], [3, 4]])
-    p = np.array([[12, 11], [2, 4]])
-
-    def add(self,A,B):
-        print(f'행렬A+행렬B: {(A+B)}')
-
-    def sub(self,A,B):
-        print(f'행렬A-행렬B: {(A-B)}')
-
-    def main(self):
-        X= cal()
-        X.add(self.r, self.p)
-        X.sub(self.r, self.p)
-
     # Basic use of websocket.
     def run(self):
         logger = self.setup_logger()
